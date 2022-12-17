c0571217e708cecc1b17988b380c1324266b7f88
Vasiliy <arapov95vasiliy@gmail.com>
2022-12-17 22:31:46 +0300

    Done my HW

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿// 1 задача.
+// Console.Clear();
+// Console.Write("Input first number: ");
+// int num1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Input second number: ");
+// int num2 = int.Parse(Console.ReadLine()!);
+
+// if(num1 > num2)
+// {
+//     Console.Write($"max = {num1}, min = {num2}");
+// }
+// else if(num1 < num2)
+// {
+//     Console.Write($"max = {num2}, min = {num1}");
+// }
+// else
+// {
+//     Console.Write($"{num1} = {num2}");
+// }
+
+// 2 задача.
+// Console.Clear();
+// Console.Write("Input first number: ");
+// int num1 = int.Parse(Console.ReadLine()!);
+// Console.Write("Input second number: ");
+// int num2 = int.Parse(Console.ReadLine()!);
+// Console.Write("Input third number: ");
+// int num3 = int.Parse(Console.ReadLine()!);
+
+// if(num1 > num2)
+// {
+//     if(num1 > num3)
+//     {
+//         Console.Write($"{num1}");
+//     }
+//     else
+//     {
+//         Console.Write($"{num3}");
+//     }
+// }
+// else if(num1 > num3)
+// {
+//     if(num1 > num2)
+//     {
+//         Console.Write($"{num1}");
+//     }
+//     else
+//     {
+//         Console.Write($"{num2}");
+//     }
+// }
+// else if(num2 > num3)
+// {
+//     if(num2 > num1)
+//     {
+//         Console.Write($"{num2}");
+//     }
+//     else
+//     {
+//         Console.Write($"{num1}");
+//     }
+// }
+// else if(num2 > num1)
+// {
+//     if(num2 > num3)
+//     {
+//         Console.Write($"{num2}");
+//     }
+//     else
+//     {
+//         Console.Write($"{num3}");
+//     }
+// }
+// else if(num3 > num1)
+// {
+//     if(num3 > num2)
+//     {
+//         Console.Write($"{num3}");
+//     }
+//     else
+//     {
+//         Console.Write($"{num2}");
+//     }
+// }
+// else
+// {
+//     Console.Write($"{num1} = {num2} = {num3}");
+// }
+
+// 3 задача.
+// Console.Clear();
+// Console.Write("Input first number: ");
+// int num1 = int.Parse(Console.ReadLine()!);
+
+// if(num1 % 2 == 0)
+// {
+//     Console.Write("Yes");
+// }
+// else
+// {
+//     Console.Write("No");
+// }
+
+// 4 задача.
+// Console.Clear();
+// Console.Write("Input first number: ");
+// int num1 = int.Parse(Console.ReadLine()!);
+// int start_num = 2;
+
+// while(start_num <= num1)
+// {
+//     Console.Write($"{start_num} ");
+//     start_num += 2;
+// }
